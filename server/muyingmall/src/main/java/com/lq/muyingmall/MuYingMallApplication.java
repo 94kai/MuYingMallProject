2db1838474615d5b94432e358c785e0e7003502f
xuekai1 <xuekai1@jd.com>
2019-01-16 20:47:24 +0800

    初始化项目

---
+++ server/muyingmall/src/main/java/com/lq/muyingmall/MuYingMallApplication.java
@@ -0,0 +1,14 @@
+package com.lq.muyingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuYingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuYingMallApplication.class, args);
+	}
+
+}
+
