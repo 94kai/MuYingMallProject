153b8b23efae252d0d9736cc02e33758305172e2
xuekai1 <xuekai1@jd.com>
2019-01-25 19:15:14 +0800

    [client]detail

--- client/mu_ying_mall/lib/view/productdetail/detail_list_view.dart
+++ client/mu_ying_mall/lib/view/productdetail/detail_list_view.dart
@@ -69,18 +69,67 @@ class DetailListViewState extends State<DetailListView> {
           color: Colors.pink,
         ),
       );
-    } else {
+    } else if (index == 1) {
       return Container(
+        padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
         color: Colors.white,
         height: 100,
         child: Column(
           children: <Widget>[
-            Text(data['title']),
-            Text(data['details']),
+            Container(
+              padding: EdgeInsets.only(bottom: 10),
+              alignment: AlignmentDirectional.centerStart,
+              child: Text(
+                data['title'],
+                maxLines: 1,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.start,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "￥",
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "${data['original_price']}",
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                Text(
+                  "已售${data['sell_num']}件  ",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Row(children: _buildGoodsTag(data['goods_tag'])),
+              ],
+            ),
           ],
         ),
       );
-      return Text("data");
+    } else if (index == 2) {
+      return Container(
+        color: Colors.black12,
+        height: 8,
+      );
     }
   }
 
@@ -102,5 +151,23 @@ class DetailListViewState extends State<DetailListView> {
       }
     }
     return true;
+  }
+
+  ///构建goodstag（包邮、运费险等）
+  _buildGoodsTag(goodsTag) {
+    return goodsTag
+        .map<Widget>((tag) => Row(
+              children: <Widget>[
+                Icon(Icons.bookmark_border,color: Color(0xFFFF8A80),)
+                ,
+                Text(
+                  tag,
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                )
+              ],
+            ))
+        .toList();
   }
 }
